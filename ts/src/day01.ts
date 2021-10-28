69bec4b23674b3ccdee9baf629fb2052ff8a42fc
Steven Hunt <sph@commandersoft.co.uk>
2021-10-28 10:31:43 +0100

    Add day 1 from 2020 as a test

---
+++ ts/src/day01.ts
@@ -0,0 +1,38 @@
+import { DayBase } from './day-runner';
+
+export class Day01 extends DayBase {
+    constructor() {
+        super(1);
+    }
+
+    public async run(): Promise<void> {
+        const lines = await this.getInputInts();
+        //console.log(`Day ${this.day} lines`, lines);
+        let part1 = 0, part2 = 0;
+
+        const target = 2020;
+        const len = lines.length;
+        for (let i = 0; i < len; i++) {
+            for (let j = i + 1; j < len; j++) {
+                if (!part1) {
+                    if (lines[i] + lines[j] === target) {
+                        part1 = lines[i] * lines[j];
+                    }
+                }
+                if (!part2) {
+                    for (let k = j + 1; k < len; k++) {
+                        if (lines[i] + lines[j] + lines[k] === target) {
+                            part2 = lines[i] * lines[j] * lines[k];
+                            break;
+                        }
+                    }
+                }
+            }
+            if (part1 && part2) break;
+        }
+
+        console.info(`Day ${this.day}`);
+        console.info(`  Part 1:`, part1);
+        console.info(`  Part 2:`, part2);
+    }
+}
