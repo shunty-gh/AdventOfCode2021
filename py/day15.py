b9bc6a0b0233692faf6f8e3e5049ec5ccb2fa8df
Steven Hunt <sph@shunty.co.uk>
2021-12-15 13:48:20 +0000

    day 15 (py) - improved

--- py/day15.py
+++ py/day15.py
@@ -1,82 +1,64 @@
 from __future__ import annotations
 import os
 import sys
-from collections import deque, Counter, defaultdict
+import heapq
+from collections import defaultdict
 
 # https://adventofcode.com/2021/day/15
 
 def expandMap(map, expandBy):
-
-    newmap = []
+    lm = len(map) # assume map is square
+    result = []
     # expand the existing rows to the new width
     for row in map:
-        newrow = []
-        newrow.extend(row)
-        thisrow = row
-        for i in range(expandBy - 1):
-            next = [max(1, (x+1) % 10) for x in thisrow]
-            newrow.extend(next)
-            thisrow = next
-        newmap.append(newrow)
+        newrow = row.copy()
+        for _ in range(expandBy - 1):
+            newrow.extend([max(1, (x+1) % 10) for x in newrow[-lm:]])
+        result.append(newrow)
 
     # expand downwards
-    lm = len(map)
-    for i in range(expandBy - 1):
-        for y in range(lm):
-            baserow = newmap[-lm]
-            newrow = [max(1, (x+1) % 10) for x in baserow]
-            newmap.append(newrow)
+    for _ in range(expandBy - 1):
+        for _ in range(lm):
+            result.append([max(1, (x+1) % 10) for x in result[-lm]])
 
-    return newmap
+    return result
 
 def getNeighbours(x, y, max):
     dd = [[1,0], [0,-1], [-1,0], [0,1]]
     return [[x+dx,y+dy] for dx,dy in dd if x+dx >= 0 and x+dx < max and y+dy >= 0 and y+dy < max]
 
-def findShortest(map, target):
-    # assume target == target X === target Y and start is (0,0) and map is square
-    maxlen = len(map)
-#    costs = defaultdict(lambda: sys.maxsize)
-    costs = {}
-    # pre-set manhattan distance for each element
-    yc = 0
-    for y in range(maxlen):
-        xc = 0
-        for x in range(maxlen):
-            xc += map[y][x]
-            costs[(x,y)] = yc + xc
-        yc += map[y][0]
-
-    toVisit = deque()
-    toVisit.append([0,0,0])
-    #shortest = sum(x for x in map[0]) + sum(y[0] for y in map) - map[0][-1] # Manhattan distance as an estimated max starting distance
-    shortest = costs[(target,target)]
+def findShortest(map):
+    # assume target is bottom right, start is (0,0) and map is square
+    mlen = len(map)
+    target = mlen - 1
+    costs = defaultdict(lambda: sys.maxsize)
+    costs[(0,0)] = 0
+    toVisit = [(0,0,0)]
+    #shortest = sum(x for x in map[0]) + sum(y[0] for y in map) - map[0][-1] # Manhattan distance as an estimated initial distance. Makes very little (if any) difference to overall runtime.
+    shortest = sys.maxsize
     while toVisit:
-        [x,y,c] = toVisit.popleft()
-        if c >= shortest or costs[(x,y)] < c:
+        c,x,y = heapq.heappop(toVisit)
+        if c >= shortest or c > costs[(x,y)]:
             continue
 
-        costs[(x,y)] = c
-
-        neigh = getNeighbours(x, y, maxlen)
-        for [nx, ny] in neigh:
-            v = map[ny][nx]
-            cost = c + v
+        neigh = getNeighbours(x, y, mlen)
+        for [nx,ny] in neigh:
+            cost = c + map[ny][nx]
             if cost > shortest:
                 continue
 
-            if costs[(nx,ny)] > cost:
+            if cost < costs[(nx,ny)]:
                 costs[(nx,ny)] = cost
                 if nx == target and ny == target:
                     shortest = cost
                 else:
-                    toVisit.append([nx, ny, cost])
+                    heapq.heappush(toVisit, (cost, nx, ny))
     return shortest
 
 with open(os.path.dirname(os.path.realpath(__file__)) + "/../input/day15-input", "r") as f:
     input = [line.strip() for line in f.readlines()]
 
-# input = [
+# input = [              # expect P1: 40; P2: 315
 #     '1163751742',
 #     '1381373672',
 #     '2136511328',
@@ -93,7 +75,7 @@ for line in input:
     map.append([int(x) for x in line])
 
 print("Day 15")
-print("  Part 1", findShortest(map, len(map[0]) - 1))
+print("  Part 1", findShortest(map))
 
 map = expandMap(map, 5)
-print("  Part 2", findShortest(map, len(map[0]) - 1))
+print("  Part 2", findShortest(map))
